e6ef9c4b0bc448d871018af98eb6f71b11ad1768
owenmerry <merryowen@gmail.com>
2023-04-21 15:32:04 +0100

    added logger to api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HttpModule } from '@nestjs/axios';
 import { ConfigModule } from '@nestjs/config';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { AppLoggerMiddleware } from './middleware/logger.middleware';
 import { join } from 'path';
 
 @Module({
@@ -23,4 +24,10 @@ import { join } from 'path';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AppLoggerMiddleware)
+      .forRoutes('*');
+  }
+}
