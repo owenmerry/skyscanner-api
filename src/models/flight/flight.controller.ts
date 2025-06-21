fed798f9fd34427db0011d3cfc50e8567619c050
owenmerry <merryowen@gmail.com>
2025-06-21 16:02:18 +0100

    changed to errors

--- src/models/flight/flight.controller.ts
+++ src/models/flight/flight.controller.ts
@@ -52,7 +52,7 @@ export class FlightController {
       mode?: 'complete';
     },
   ): Promise<any> {
-    return false;
+    throw new Error('Turned off for now');
     const searchHash = await this.flightService.createHash({
       ...query,
       returnDate: query.return,
@@ -99,7 +99,7 @@ export class FlightController {
       mode?: 'complete';
     },
   ): Promise<any> {
-    return false;
+    throw new Error('Turned off for now');
     const res = await this.flightService.flightsLivePricesPoll(params.token);
 
     if (query.from && res.data.status === 'RESULT_STATUS_COMPLETE') {
@@ -140,6 +140,7 @@ export class FlightController {
       groupType?: string;
     },
   ): Promise<any> {
+    throw new Error('Turned off for now');
     console.log('check', {
       endMonth: query.endMonth,
       endYear: query.endYear,
@@ -160,7 +161,7 @@ export class FlightController {
       return: string;
     },
   ): Promise<any> {
-    return false;
+    throw new Error('Turned off for now');
     const searchHash = await this.flightService.createHash({
       ...query,
       returnDate: query.return,
@@ -213,6 +214,7 @@ export class FlightController {
       entityId: string;
     },
   ): Promise<any> {
+        throw new Error('Turned off for now');
     const res = await this.flightService.searchHotels(query);
     const data = res.data;
 
@@ -227,6 +229,7 @@ export class FlightController {
       query: string;
     },
   ): Promise<any> {
+        throw new Error('Turned off for now');
     // on your node server
     const serverApi = createApi({
       accessKey: this.UNSPLASH_ACCESS_KEY,
